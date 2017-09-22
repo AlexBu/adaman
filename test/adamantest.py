961bacb421c0112f9687d848d3d3c3e3c02da308
Alex Bu <bkpfx000@gmail.com>
2017-09-22 15:55:34 +0800

    update test data path

--- test/adamantest.py
+++ test/adamantest.py
@@ -10,7 +10,7 @@ class ArchiveTest(unittest.TestCase):
 
     def setUp(self):
         self.name_list = ['1.lzs', '2.lzs', '3.lzs', 'Credits.tim', 'discerr.lzs', 'discimg1.lzs', 'discimg2.lzs', 'discimg3.lzs', 'discimg4.lzs', 'harata.cnf', 'icon.tim', 'init.out', 'kernel.bin', 'namedic.bin', 'sysfnt.tdw', 'wm2field.tbl', 'ff8.lzs', 'loop01.lzs', 'loop02.lzs', 'loop03.lzs', 'loop04.lzs', 'loop05.lzs', 'loop06.lzs', 'loop07.lzs', 'loop08.lzs', 'loop09.lzs', 'loop10.lzs', 'loop11.lzs', 'loop12.lzs', 'loop13.lzs', 'loop14.lzs', 'name01.lzs', 'name02.lzs', 'name03.lzs', 'name04.lzs', 'name05.lzs', 'name06.lzs', 'name07.lzs', 'name08.lzs', 'name09.lzs', 'name10.lzs', 'name11.lzs', 'name12.lzs', 'name13.lzs', 'name14.lzs', 'square.lzs' ]
-        self.ar = main.Archive('main')
+        self.ar = main.Archive('../testdata/main')
 
     def tearDown(self):
         pass
@@ -27,7 +27,7 @@ class ArchiveTest(unittest.TestCase):
 
 class ArchiveFolderTest(unittest.TestCase):
     def setUp(self):
-        self.af = main.ArchiveFolder('.')
+        self.af = main.ArchiveFolder('../testdata')
 
     def tearDown(self):
         pass
