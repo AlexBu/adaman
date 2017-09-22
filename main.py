bab7a4133c3465f194cda0b636036879a175de23
Alex Bu <bkpfx000@gmail.com>
2017-09-22 10:16:35 +0800

    update implementation and test case, extract for one level deep folder added

--- main.py
+++ main.py
@@ -1,2 +1,106 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
+import os
+import sys
+import struct
+import clzs
+import glob
+
+class Archive():
+    def __init__(self, name):
+        self._file_info = dict(name = name, count = 0, entries = [])
+        self._load_fl()
+        self._load_fi()
+        self._load_fs()
+
+    def __str__(self):
+        info = "archive info\n"
+        info += self._get_name()
+        info += self._get_entry()
+        return info
+
+    def _get_name(self):
+        return "name: %s\n" % self._file_info['name']
+
+    def _get_entry(self):
+        total_str = 'entries \n'
+        for entry in self._file_info['entries']:
+            entry_str = 'entry name = %s,\tactual size = %d, encoded size = %d, offset = %x\n'\
+                        % (entry['name'], entry['act_size'], entry['enc_size'], entry['offset'])
+            total_str += entry_str
+        return total_str
+
+    def _load_fl(self):
+        fl = open(self._file_info['name'] + '.fl')
+        for line in fl.readlines():
+            self._file_info['entries'].append(dict(name = line.split('\\')[-1].rstrip()))
+        self._file_info['count'] = len(self._file_info['entries'])
+
+    def get_list(self):
+        return [entry['name'] for entry in self._file_info['entries']]
+
+    def get_index(self):
+        return [entry['name'] for entry in self._file_info['entries']]
+
+    def _load_fi(self):
+        fi = open(self._file_info['name'] + '.fi').read()
+        for i in range(self._file_info['count']):
+            (size, offset, lzs) = struct.unpack("<3I", fi[i*12 : (i + 1)*12])
+            self._file_info['entries'][i]['act_size'] = size
+            self._file_info['entries'][i]['offset'] = offset
+            self._file_info['entries'][i]['lzsed'] = (lzs == 1)
+
+    def _load_fs(self):
+        previous = os.path.getsize(self._file_info['name'] + '.fs')
+        for i in range(self._file_info['count'] - 1, -1, -1):
+            self._file_info['entries'][i]['enc_size'] = previous - self._file_info['entries'][i]['offset']
+            previous = self._file_info['entries'][i]['offset']
+
+    def create_subfolder(self, name):
+        if not os.path.exists(name):
+            os.makedirs(name)
+
+    def create_segments(self):
+        self.create_subfolder(self._file_info['name'])
+
+        fs = open(self._file_info['name'] + '.fs')
+        for entry in self._file_info['entries']:
+            if entry['lzsed'] == True:
+                fs.seek(entry['offset'])
+                enc_data = fs.read(entry['enc_size'])
+                sub_name = './' + self._file_info['name'] + '/' + entry['name']
+                tmp_name = sub_name + '.tmp'
+                tmp = open(tmp_name, 'wb')
+                tmp.write(enc_data)
+                tmp.close()
+                decode_result = clzs.decode_file(tmp_name, sub_name)
+                assert (decode_result == 0)
+                os.remove(tmp_name)
+            else:
+                fs.seek(entry['offset'])
+                enc_data = fs.read(entry['enc_size'])
+                assert (entry['enc_size'] == entry['act_size'])
+                sub_name = './' + self._file_info['name'] + '/' + entry['name']
+                tmp = open(sub_name, 'wb')
+                tmp.write(enc_data)
+                tmp.close()
+        fs.close()
+
+class ArchiveFolder():
+    def __init__(self, dir):
+        self.archive_list = map(lambda one_file: os.path.splitext(os.path.basename(one_file))[0],  glob.glob(os.path.join(dir, '*.fs')))
+
+    def __str__(self):
+        return 'find %d archives in the folder %s\n' % (len(self.archive_list), self.archive_list)
+
+    def extract(self):
+        for entry in self.archive_list:
+            ar = Archive(entry)
+            ar.create_segments()
+
+if __name__ == '__main__':
+    print sys.argv[1]
+    ar = Archive(sys.argv[1])
+    print ar
+    pass
--- test/adamantest.py
+++ test/adamantest.py
@@ -2,15 +2,42 @@
 # encoding: utf-8
 
 import unittest
+import main
+import filecmp
 
-class LzsTest(unittest.TestCase):
+
+class ArchiveTest(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.name_list = ['1.lzs', '2.lzs', '3.lzs', 'Credits.tim', 'discerr.lzs', 'discimg1.lzs', 'discimg2.lzs', 'discimg3.lzs', 'discimg4.lzs', 'harata.cnf', 'icon.tim', 'init.out', 'kernel.bin', 'namedic.bin', 'sysfnt.tdw', 'wm2field.tbl', 'ff8.lzs', 'loop01.lzs', 'loop02.lzs', 'loop03.lzs', 'loop04.lzs', 'loop05.lzs', 'loop06.lzs', 'loop07.lzs', 'loop08.lzs', 'loop09.lzs', 'loop10.lzs', 'loop11.lzs', 'loop12.lzs', 'loop13.lzs', 'loop14.lzs', 'name01.lzs', 'name02.lzs', 'name03.lzs', 'name04.lzs', 'name05.lzs', 'name06.lzs', 'name07.lzs', 'name08.lzs', 'name09.lzs', 'name10.lzs', 'name11.lzs', 'name12.lzs', 'name13.lzs', 'name14.lzs', 'square.lzs' ]
+        self.ar = main.Archive('main')
 
     def tearDown(self):
         pass
 
+    def test_list_loading(self):
+        print self.ar
+        new_list = self.ar.get_list()
+        self.assertEquals(new_list, self.name_list)
+
+    @unittest.skip("not to touch files and folders")
+    def test_data_loadin(self):
+        self.ar.create_segments()
+        compare_result = filecmp.dircmp('./main', './expected/main')
+        self.assertEquals(len(compare_result.diff_files), 0)
+
+class ArchiveFolderTest(unittest.TestCase):
+    def setUp(self):
+        self.af = main.ArchiveFolder('.')
+
+    def tearDown(self):
+        pass
+
+    def test_find_archive(self):
+        print self.af
+
+    def test_extract_archive(self):
+        self.af.extract()
 
 if __name__ == '__main__':
     unittest.main()
